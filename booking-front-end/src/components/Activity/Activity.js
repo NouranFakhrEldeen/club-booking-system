81e1f4ffcae3a8146f69a0788448d3a60314faa3
Amir Hesham <amir.heham@vodafone.com>
2021-05-27 13:56:15 +0200

    add activity details section

--- booking-front-end/src/components/Activity/Activity.js
+++ booking-front-end/src/components/Activity/Activity.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Icon from 'react-web-vector-icons';
 
 import "./Activity.scss";
+import i18n from '../../i18n';
 
 const Activity=(props)=>{
-  const {image,title,description}=props
+  const {image,title,description,isActivityScreen,cost,time}=props
+
+  const renderButton=()=>{
+    if(!isActivityScreen){
+      return (
+        <div className="activityBottomDetails">
+          <p className="details">{cost} EGP</p>
+          <div className="seperator"/>
+          <div className="hourDetails">
+            <Icon
+              name='time-outline'
+              font='ionicons'
+              color='green'
+              size={20}
+              // style={{}}
+            />
+            <p className="details">{time}</p>
+
+          </div>
+
+        </div>
+      )
+    }
+    return (
+      <button className="button">
+        {i18n.t('activity.viewActivity')}
+      </button>
+    )
+  }
   return(
     <div className="mainContainer">
       <img src={image}
@@ -14,17 +44,25 @@ const Activity=(props)=>{
       <div className="inner-container" >
         <p className="title">{title} </p>
         <p className="description">{description}</p>
-        <button className="button">
-        View Activity
-        </button>
       </div>
-  
+      {renderButton()}
+
     </div>
   )
 }
+
+Activity.defaultProps={
+  isActivityScreen:true,
+  cost:0,
+  time:null
+}
+
 Activity.proptypes={
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
+  description: PropTypes.string.isRequired,
+  isActivityScreen: PropTypes.bool,
+  cost: PropTypes.number,
+  time: PropTypes.string
 }
 export default Activity
--- booking-front-end/src/index.js
+++ booking-front-end/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+require('react-web-vector-icons/fonts');
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
